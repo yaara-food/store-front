d176caf4c73d8f7a54845fc2b74e30eb73b03b9b
giladfuchs <igilfu@gmail.com>
2025-05-19 17:01:04 +0300

    🚀 Deploy frontend

--- lib/types/form.ts
+++ lib/types/form.ts
@@ -5,125 +5,102 @@ import { Image } from "./entities";
 
 export class FieldInput {
   constructor(
-    public type: FormType = FormType.TEXT,
-    public key: string,
-    public value: string = "",
+      public type: FormType,
+      public key: string,
+      public value: string | boolean = "",
   ) {}
 }
 
 export class FieldAutoComplete {
   readonly type: FormType = FormType.AutoComplete;
+  public value: string;
 
   constructor(
-    public key: string,
-    public options: string[],
-    public value: string = options[0] ?? "",
-  ) {}
+      public key: string,
+      public options: string[],
+      value?: string,
+  ) {
+    this.value = value ?? options[0] ?? "";
+  }
 }
 
 export class FieldImages {
   readonly type: FormType = FormType.ImagesEditor;
 
   constructor(
-    public key: string,
-    public value: Image[] = [],
+      public key: string,
+      public value: Image[] = [],
   ) {}
 }
 
 export type FormField = FieldAutoComplete | FieldInput | FieldImages;
 
-export type InputField = {
-  key: string;
-  type: FormType;
-  value?: string | boolean;
-  options?: string[];
-};
-
-export const product_fields: InputField[] = [
-  { key: "title", type: FormType.TEXT },
-  { key: "price", type: FormType.NUMBER },
-  { key: "description", type: FormType.TEXTAREA },
-  {
-    key: "category",
-    type: FormType.AutoComplete,
-    options: [],
-  },
-  { key: "available", type: FormType.Switch, value: true },
-  { key: "images", type: FormType.ImagesEditor },
+export const product_fields: FormField[] = [
+  new FieldInput(FormType.TEXT, "title"),
+  new FieldInput(FormType.NUMBER, "price"),
+  new FieldInput(FormType.TEXTAREA, "description"),
+  new FieldAutoComplete("category", []),
+  new FieldInput(FormType.Switch, "available", true),
+  new FieldImages("images", []),
 ];
-export const category_fields: InputField[] = [
-  { key: "title", type: FormType.TEXT },
-  { key: "position", type: FormType.NUMBER },
+
+export const category_fields: FormField[] = [
+  new FieldInput(FormType.TEXT, "title"),
+  new FieldInput(FormType.NUMBER, "position"),
 ];
-type InputFormType = FormType.TEXT | FormType.TEXTAREA | FormType.NUMBER;
-
-const isInputFormType = (type: FormType): type is InputFormType => {
-  return (
-    type === FormType.TEXT ||
-    type === FormType.TEXTAREA ||
-    type === FormType.NUMBER
-  );
-};
-export const json_field_to_form_field = (json_field: InputField): FormField => {
-  if (isInputFormType(json_field.type)) {
-    return new FieldInput(json_field.type, json_field.key);
-  }
-  if (json_field.type === FormType.Switch) {
-    return new FieldInput(
-      json_field.type,
-      json_field.key,
-      json_field.value as string,
-    );
-  }
-
-  if (json_field.type === FormType.AutoComplete) {
-    return new FieldAutoComplete(json_field.key, json_field.options!);
-  }
-  if (json_field.type === FormType.ImagesEditor) {
-    return new FieldImages(json_field.key);
-  }
-
-  return new FieldInput(json_field.type, json_field.key);
-};
-
-export const json_fields_to_form_fields = (
-  json_fields: InputField[],
-): FormField[] =>
-  json_fields.map((obj: InputField) => json_field_to_form_field(obj));
 
 export const get_form_by_model = (type_form: ModelType): FormField[] => {
   switch (type_form) {
     case ModelType.product:
-      return json_fields_to_form_fields([...product_fields]);
+      return [...product_fields];
     case ModelType.category:
-      return json_fields_to_form_fields([...category_fields]);
     default:
-      return json_fields_to_form_fields([...category_fields]);
+      return [...category_fields];
   }
 };
+
 export const array_obj_to_obj_with_key = (
-  iterable: any[],
-  value: any,
-  key: string,
+    iterable: any[],
+    value: any,
+    key: string,
 ) => iterable.find((o: any) => o[key]?.toString() === value.toString());
+
 export const create_form_fields = (
-  source: FormField[],
-  target: any,
+    source: FormField[],
+    target: any,
 ): FormField[] => {
   if (
-    target == null ||
-    (Array.isArray(target) && target.length === 0) ||
-    (typeof target === "object" && Object.keys(target).length === 0)
-  )
-    return source.map((field) => ({ ...field }));
-  return source.map((field) => {
-    let updatedValue = target[field.key];
+      target == null ||
+      (Array.isArray(target) && target.length === 0) ||
+      (typeof target === "object" && Object.keys(target).length === 0)
+  ) {
+    return source.map((field) => {
+      if (field instanceof FieldImages) {
+        return new FieldImages(field.key, field.value);
+      }
+      if (field instanceof FieldAutoComplete) {
+        return new FieldAutoComplete(field.key, field.options, field.value);
+      }
+      return new FieldInput(field.type, field.key, field.value);
+    });
+  }
 
-    if (typeof updatedValue === "string") {
-      updatedValue = updatedValue.trim();
+  return source.map((field) => {
+    const updatedValue = target[field.key];
+
+    if (field instanceof FieldImages && Array.isArray(updatedValue)) {
+      return new FieldImages(field.key, updatedValue);
     }
-    return { ...field, value: updatedValue };
+
+    if (field instanceof FieldAutoComplete) {
+      return new FieldAutoComplete(field.key, field.options, updatedValue);
+    }
+
+    return new FieldInput(
+        field.type,
+        field.key,
+        typeof updatedValue === "string" ? updatedValue.trim() : updatedValue,
+    );
   });
 };
-
 /* eslint-enable max-classes-per-file */
