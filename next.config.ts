bddd6a9e73f55384c37bcc728a7bda16369e3ba0
giladfuchs <igilfu@gmail.com>
2025-07-20 12:35:35 +0300

    🚀 Deploy frontend

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
   },
 
   images: {
-    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       { protocol: "https", hostname: "**" },
